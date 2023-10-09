91b0365b429eb00608225630a539866dfb2c6f3d
Amancayjesus136 <amancayjesus136@gmail.com>
2023-10-09 17:28:16 -0500

    falta aviso de no tienes rol

---
+++ resources/views/esperar-superadmin.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+    <title>Esperar al Super Administrador</title>
+    <div>
+        <h1>Esperar al Super Administrador</h1>
+        <p>Por favor, espera a que un Super Administrador te asigne un rol para acceder al sistema.</p>
+    </div>
+</html>
+@endsection
