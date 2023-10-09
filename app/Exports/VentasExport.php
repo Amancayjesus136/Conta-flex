ddf974669c813cda1eac675cb494bcbd0ea0337b
Amancayjesus136 <amancayjesus136@gmail.com>
2023-10-09 04:26:09 -0500

    falta cabeceras al excel

--- app/Exports/VentasExport.php
+++ app/Exports/VentasExport.php
@@ -3,11 +3,30 @@ namespace App\Exports;
 
 use App\Models\ventas;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
 class VentasExport implements FromCollection
 {
     public function collection()
     {
         return ventas::all();
+    }
+    public function headings(): array
+    {
+        return [
+            'RUC',
+            'Nombre del Proveedor',
+            'Cod Compra',
+            'Tipo de Cambio',
+            'Documento',
+            'Factura Número',
+            'Base Disponible',
+            'Tasa de IGV',
+            'IGV',
+            'Total',
+            'Fecha Comprobante',
+            'Fecha de Emisión',
+            'Fecha de Venta',
+        ];
     }
 }
--- app/Http/Controllers/ReporteVentasController.php
+++ app/Http/Controllers/ReporteVentasController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\ventas;
 use App\Exports\VentasExport;
-use Excel;
+use Maatwebsite\Excel\Facades\Excel;
 
 class ReporteVentasController extends Controller
 {
