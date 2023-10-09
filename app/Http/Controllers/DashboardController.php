fbdc3acc5d13b505a7d0a1c951e330e5222d9843
Amancayjesus136 <amancayjesus136@gmail.com>
2023-10-09 17:15:23 -0500

    falta mejorar inicio

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\compras;
+
+
+class DashboardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+{
+    // Obtén el conteo total de registros en la tabla compras
+    $totalCompras = compras::count();
+
+    // Pasa el total de compras a la vista
+    return view('dashboard', compact('totalCompras'));
+}
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,90 +2,87 @@
 
 
 @section('content')
-
-
-                    <div class="row project-wrapper">
-                        <div class="col-xxl-8">
-                            <div class="row">
-                                <div class="col-xl-4">
-                                    <div class="card card-animate">
-                                        <div class="card-body">
-                                            <div class="d-flex align-items-center">
-                                                <div class="avatar-sm flex-shrink-0">
-                                                    <span class="avatar-title bg-soft-primary text-primary rounded-2 fs-2">
-                                                        <i data-feather="briefcase" class="text-primary"></i>
-                                                    </span>
-                                                </div>
-                                                <div class="flex-grow-1 overflow-hidden ms-3">
-                                                    <p class="text-uppercase fw-medium text-muted text-truncate mb-3">Active Projects</p>
-                                                    <div class="d-flex align-items-center mb-3">
-                                                        <h4 class="fs-4 flex-grow-1 mb-0"><span class="counter-value" data-target="825">0</span></h4>
-                                                        <span class="badge badge-soft-danger fs-12"><i class="ri-arrow-down-s-line fs-13 align-middle me-1"></i>5.02 %</span>
-                                                    </div>
-                                                    <p class="text-muted text-truncate mb-0">Projects this month</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
+<div class="row project-wrapper">
+    <div class="col-xxl-8">
+        <div class="row">
+            <div class="col-xl-4">
+                <div class="card card-animate">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center">
+                            <div class="avatar-sm flex-shrink-0">
+                                <span class="avatar-title bg-soft-primary text-primary rounded-2 fs-2">
+                                    <i data-feather="briefcase" class="text-primary"></i>
+                                </span>
+                            </div>
+                            <div class="flex-grow-1 overflow-hidden ms-3">
+                                <p class="text-uppercase fw-medium text-muted text-truncate mb-3">Active Projects</p>
+                                <div class="d-flex align-items-center mb-3">
+                                    <h4 class="fs-4 flex-grow-1 mb-0"><span class="counter-value" data-target="825">0</span></h4>
+                                    <span class="badge badge-soft-danger fs-12"><i class="ri-arrow-down-s-line fs-13 align-middle me-1"></i>5.02 %</span>
                                 </div>
-
-                                <div class="col-xl-4">
-                                    <div class="card card-animate">
-                                        <div class="card-body">
-                                            <div class="d-flex align-items-center">
-                                                <div class="avatar-sm flex-shrink-0">
-                                                    <span class="avatar-title bg-soft-warning text-warning rounded-2 fs-2">
-                                                        <i data-feather="award" class="text-warning"></i>
-                                                    </span>
-                                                </div>
-                                                <div class="flex-grow-1 ms-3">
-                                                    <p class="text-uppercase fw-medium text-muted mb-3">New Leads</p>
-                                                    <div class="d-flex align-items-center mb-3">
-                                                        <h4 class="fs-4 flex-grow-1 mb-0"><span class="counter-value" data-target="7522">0</span></h4>
-                                                        <span class="badge badge-soft-success fs-12"><i class="ri-arrow-up-s-line fs-13 align-middle me-1"></i>3.58 %</span>
-                                                    </div>
-                                                    <p class="text-muted mb-0">Leads this month</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="col-xl-4">
-                                    <div class="card card-animate">
-                                        <div class="card-body">
-                                            <div class="d-flex align-items-center">
-                                                <div class="avatar-sm flex-shrink-0">
-                                                    <span class="avatar-title bg-soft-info text-info rounded-2 fs-2">
-                                                        <i data-feather="clock" class="text-info"></i>
-                                                    </span>
-                                                </div>
-                                                <div class="flex-grow-1 overflow-hidden ms-3">
-                                                    <p class="text-uppercase fw-medium text-muted text-truncate mb-3">Fecha & Hora</p>
-                                                    <div class="d-flex align-items-center mb-3">
-                                                        <h4 class="fs-4 flex-grow-1 mb-0">
-                                                            <span id="reloj"></span>
-                                                        </h4>
-                                                    </div>
-                                                    <p class="text-muted text-truncate mb-0"><span id="fecha"></span></p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                            <!-- <div class="row">
-                                <div class="col-xl-12">
-                                    <div class="card">
-                                    <img src="{{ asset('assets/images/logo2.jpg') }}" alt="" height="60" class="img-fluid">
-                                    </div>
-                                </div>
-                            </div> -->
+                                <p class="text-muted text-truncate mb-0">Projects this month</p>
+                            </div>
                         </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xl-4">
+                <div class="card card-animate">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center">
+                            <div class="avatar-sm flex-shrink-0">
+                                <span class="avatar-title bg-soft-warning text-warning rounded-2 fs-2">
+                                    <i data-feather="award" class="text-warning"></i>
+                                </span>
+                            </div>
+                            <div class="flex-grow-1 ms-3">
+                                <p class="text-uppercase fw-medium text-muted mb-3">New Leads</p>
+                                <div class="d-flex align-items-center mb-3">
+                                    <h4 class="fs-4 flex-grow-1 mb-0"><span class="counter-value" data-target="7522">0</span></h4>
+                                    <span class="badge badge-soft-success fs-12"><i class="ri-arrow-up-s-line fs-13 align-middle me-1"></i>3.58 %</span>
+                                </div>
+                                <p class="text-muted mb-0">Leads this month</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xl-4">
+                <div class="card card-animate">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center">
+                            <div class="avatar-sm flex-shrink-0">
+                                <span class="avatar-title bg-soft-info text-info rounded-2 fs-2">
+                                    <i data-feather="clock" class="text-info"></i>
+                                </span>
+                            </div>
+                            <div class="flex-grow-1 overflow-hidden ms-3">
+                                <p class="text-uppercase fw-medium text-muted text-truncate mb-3">Fecha & Hora</p>
+                                <div class="d-flex align-items-center mb-3">
+                                    <h4 class="fs-4 flex-grow-1 mb-0">
+                                        <span id="reloj"></span>
+                                    </h4>
+                                </div>
+                                <p class="text-muted text-truncate mb-0"><span id="fecha"></span></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        <!-- <div class="row">
+            <div class="col-xl-12">
+                <div class="card">
+                <img src="{{ asset('assets/images/logo2.jpg') }}" alt="" height="60" class="img-fluid">
+                </div>
+            </div>
+        </div> -->
+    </div>
 
 @extends('layouts.footer')
 
-<!-- Coloca esto al final de tu vista o enlaza a un archivo JavaScript externo -->
 <script>
     function actualizarHora() {
         var ahora = new Date();
@@ -93,24 +90,18 @@
         var minutos = ahora.getMinutes();
         var segundos = ahora.getSeconds();
 
-        // Formatea los minutos y segundos para que siempre tengan dos dígitos
         minutos = minutos < 10 ? '0' + minutos : minutos;
         segundos = segundos < 10 ? '0' + segundos : segundos;
 
-        // Obtiene la fecha actual en el formato "dd/mm/yyyy"
         var fechaActual = ahora.getDate() + '/' + (ahora.getMonth() + 1) + '/' + ahora.getFullYear();
 
-        // Actualiza el contenido del elemento con el ID "reloj"
         document.getElementById('reloj').textContent = horas + ':' + minutos + ':' + segundos;
 
-        // Actualiza el contenido del elemento con el ID "fecha"
         document.getElementById('fecha').textContent = fechaActual;
     }
 
-    // Actualiza la hora y la fecha cada segundo
     setInterval(actualizarHora, 1000);
 
-    // Llama a la función una vez para mostrar la hora y la fecha iniciales inmediatamente
     actualizarHora();
 </script>
 
