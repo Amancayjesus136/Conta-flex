5f25527cdd86e8b78368823d1ecfc1f640ca68b8
Andres <sergiocrispin07@gmail.com>
2023-10-24 14:15:22 -0500

    igv-incluye

--- resources/views/reporte_ventas/index.blade.php
+++ resources/views/reporte_ventas/index.blade.php
@@ -201,10 +201,13 @@
                                         </div>
                                     </div>
                                     <div class="col-md-4">
-                                        <div class="form-group"><br>
-                                            <label for="IGV">IGV</label>
-                                            <input type="number" class="form-control" id="IGV" name="IGV" value="18">
-                                        </div>
+                                      <div class="form-group"><br>
+                                          <label for="IGV">IGV</label>
+                                          <select class="form-control" id="IGV" name="IGV">
+                                              <option value="18">IGV</option>
+                                              <option value="1.18">IGV INCLUIDO</option>
+                                          </select>
+                                      </div>
                                     </div>
                                     <div class="col-md-4">
                                         <div class="form-group"><br>
@@ -420,6 +423,43 @@
 @endforeach
 
 <script>
+    // Obtener elementos del DOM
+    const igvSelect = document.getElementById('IGV');
+    const baseDisponibleInput = document.getElementById('base_disponible');
+    const totalInput = document.getElementById('total');
+
+    // Agregar un evento de cambio al select
+    igvSelect.addEventListener('change', calcularTotal);
+    
+    // Agregar un evento de entrada al campo "base_disponible"
+    baseDisponibleInput.addEventListener('input', calcularTotal);
+
+    // Función para calcular el total
+    function calcularTotal() {
+        const selectedValue = parseFloat(igvSelect.value);
+        const baseDisponible = parseFloat(baseDisponibleInput.value);
+        let result;
+
+        if (isNaN(baseDisponible)) {
+            // Si el usuario no ingresó un número en "base_disponible", el total se establece en 0
+            result = 0;
+        } else if (selectedValue === 18) {
+            // Si se selecciona "IGV" (valor 18), realizar el cálculo
+            result = baseDisponible * 0.18;
+        } else if (selectedValue === 1.18) {
+            // Si se selecciona "IGV INCLUIDO" (valor 1.18), realizar el cálculo
+            result = (baseDisponible / 1.18) * 0.18;
+        } else {
+            // Manejar otras opciones aquí
+            result = 0; // Otra opción
+        }
+
+        // Mostrar el resultado en el campo "total"
+        totalInput.value = result.toFixed(2); // Redondear a 2 decimales
+    }
+</script>
+
+<!-- <script>
     // Obtiene los elementos de los campos de entrada
     var baseInput = document.getElementById('base_disponible');
     var igvInput = document.getElementById('IGV');
@@ -443,6 +483,6 @@
         // Actualiza el valor del campo Total
         totalInput.value = total.toFixed(2); // Limita el resultado del total a dos decimales
     }
-</script>
+</script> -->
 @endsection
 
