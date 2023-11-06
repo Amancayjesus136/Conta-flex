153b2cd5c870aa825222460fbc34b5cf0669ccfa
Amancayjesus136 <amancayjesus136@gmail.com>
2023-11-05 21:10:43 -0500

    falta css

--- app/Http/Controllers/ReporteVentasController.php
+++ app/Http/Controllers/ReporteVentasController.php
@@ -36,7 +36,7 @@ class ReporteVentasController extends Controller
             });
         }
 
-        $reporteventas = $reporteventas->paginate(5);
+        $reporteventas = $reporteventas->paginate(7);
 
         if ($request->has('export')) {
             return Excel::download(new VentasExport, 'ventas.xlsx');
