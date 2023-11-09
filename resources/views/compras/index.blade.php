fb22a95663d24348d39d4af395840a5d7f230675
Amancayjesus136 <amancayjesus136@gmail.com>
2023-11-09 03:00:05 -0500

    igv falta

--- resources/views/compras/index.blade.php
+++ resources/views/compras/index.blade.php
@@ -256,18 +256,7 @@
 
 
 
-
-
-
-
-
-
-
-
-
 <!-- animaciones -->
-
-
 <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
 <script src="assets/libs/simplebar/simplebar.min.js"></script>
 <script src="assets/libs/node-waves/waves.min.js"></script>
