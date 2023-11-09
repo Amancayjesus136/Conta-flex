742781117081e1f56cb1917e1c9fbbec03c1998b
Amancayjesus136 <amancayjesus136@gmail.com>
2023-11-09 17:20:49 -0500

    corregir icons

--- resources/views/reporte_ventas/index.blade.php
+++ resources/views/reporte_ventas/index.blade.php
@@ -9,9 +9,17 @@
 <div class="row">
     <div class="col-12">
         <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-        <a href="{{ route('reporte_ventas.index', ['export' => 1]) }}" class="btn btn-success btn-sm">
-            <i class="fas fa-file-excel"></i>  Excel
-        </a>
+            <div class="form-group">
+                <select class="form-select" id="opciones" onchange="redirectToURL(this)">
+                    <option value="">Descargar..</option>
+                    <option value="{{ route('reporte_ventas.index', ['export' => 1]) }}">
+                        <span><i class="fas fa-file-excel"></i></span> Excel
+                    </option>
+                    <option value="#">
+                        <span><i class="fas fa-file-excel"></i></span> PDF
+                    </option>
+                </select>
+            </div>
 
 
             <div class="page-title-right">
@@ -519,6 +527,17 @@
 
         document.getElementById("igv").value = igv.toFixed(2);
         document.getElementById("total").value = total.toFixed(2);
+    }
+</script>
+
+<!-- ecxel -->
+
+<script>
+    function redirectToURL(selectElement) {
+        var selectedOption = selectElement.options[selectElement.selectedIndex];
+        if (selectedOption.value !== "") {
+            window.location.href = selectedOption.value;
+        }
     }
 </script>
 @endsection
