746c968c724e6482fae97d9fbd5b148410f0a6cc
Amancayjesus136 <amancayjesus136@gmail.com>
2023-11-12 01:10:32 -0500

    PRUEBA DE CSS

--- resources/views/getpost/index.blade.php
+++ resources/views/getpost/index.blade.php
@@ -8,31 +8,31 @@
         </div>
         <div class="card-body">
         <form id="consultaForm" action="{{ route('getpost.consultarRuc') }}" method="get">
-    @csrf
-    <div class="form-group">
-        <label for="ruc">Número de RUC</label>
-        <input type="text" class="form-control" name="ruc" id="ruc" placeholder="Ingrese el RUC">
-        <button type="button" class="btn btn-primary" onclick="consultarRuc()">Consultar</button><br><br><br>
-    </div>
+            @csrf
+            <div class="form-group">
+                <label for="ruc">Número de RUC</label>
+                <input type="text" class="form-control" name="ruc" id="ruc" placeholder="Ingrese el RUC">
+                <button type="button" class="btn btn-primary" onclick="consultarRuc()">Consultar</button><br><br><br>
+            </div>
 
-    @if(isset($data))
-        <!-- ... Tu código para mostrar los resultados de la consulta API ... -->
-        <div class="row">
-            <div class="col-md-6">
-                <label for="nombre">Número de RUC</label>
-                <p name="ruc">{{ $data['numeroDocumento'] }}</p>
-                <input type="hidden" name="ruc_api" value="{{ $data['numeroDocumento'] }}">
-            </div>
-            <div class="col-md-6">
-                <label for="nombre">Nombre</label>
-                <p name="nombre">{{ $data['nombre'] }}</p>
-                <input type="hidden" name="nombre_api" value="{{ $data['nombre'] }}">
-            </div>
-        </div>
-        
-        <button type="button" class="btn btn-primary" onclick="guardar()">Guardar</button>
-    @endif
-</form>
+            @if(isset($data))
+                <!-- ... Tu código para mostrar los resultados de la consulta API ... -->
+                <div class="row">
+                    <div class="col-md-6">
+                        <label for="nombre">Número de RUC</label>
+                        <p name="ruc">{{ $data['numeroDocumento'] }}</p>
+                        <input type="hidden" name="ruc_api" value="{{ $data['numeroDocumento'] }}">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="nombre">Nombre</label>
+                        <p name="nombre">{{ $data['nombre'] }}</p>
+                        <input type="hidden" name="nombre_api" value="{{ $data['nombre'] }}">
+                    </div>
+                </div>
+                
+                <button type="button" class="btn btn-primary" onclick="guardar()">Guardar</button>
+            @endif
+        </form>
 <form id="guardarForm" action="{{ route('getpost.guardar') }}" method="post">
     @csrf
     <input type="hidden" name="ruc" id="ruc_guardar">
