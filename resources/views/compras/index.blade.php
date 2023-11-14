e0cf0bc66e3c6a1547d9bbec29570e051ca83de9
Amancayjesus136 <amancayjesus136@gmail.com>
2023-11-14 14:31:49 -0500

    hola mundo2

--- resources/views/compras/index.blade.php
+++ resources/views/compras/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin')
 
 @section('content')
-<!-- cabecera -->
+<!-- cabecera --> 
 <div class="row">
     <div class="col-12">
         <div class="page-title-box d-sm-flex align-items-center justify-content-between">
