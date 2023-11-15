4a40abb234d1d35bfcc2a779422a23e2b8c99fb5
Amancayjesus136 <amancayjesus136@gmail.com>
2023-11-15 09:59:46 -0500

    footer

--- resources/views/layouts/invitados.blade.php
+++ resources/views/layouts/invitados.blade.php
@@ -63,7 +63,7 @@
                     <div class="col-lg-12">
                         <div class="text-center">
                             <p class="mb-0 text-muted">&copy;
-                                <script>document.write(new Date().getFullYear())</script> Phenomenal.
+                                <script>document.write(new Date().getFullYear())</script> ContaFlex.
                             </p>
                         </div>
                     </div>
