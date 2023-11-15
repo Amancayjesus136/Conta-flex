cb88d170d7515117ba0b12d4632901a079cfe0b0
Amancayjesus136 <amancayjesus136@gmail.com>
2023-11-15 05:00:27 -0500

    home en duro

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,86 +2,121 @@
 
 
 @section('content')
-<div class="row project-wrapper">
-    <div class="col-xxl-8">
-        <div class="row">
-            <div class="col-xl-4">
-                <div class="card card-animate">
-                    <div class="card-body">
-                        <div class="d-flex align-items-center">
-                            <div class="avatar-sm flex-shrink-0">
-                                <span class="avatar-title bg-soft-primary text-primary rounded-2 fs-2">
-                                    <i data-feather="briefcase" class="text-primary"></i>
-                                </span>
-                            </div>
-                            <div class="flex-grow-1 overflow-hidden ms-3">
-                                <p class="text-uppercase fw-medium text-muted text-truncate mb-3">Active Projects</p>
-                                <div class="d-flex align-items-center mb-3">
-                                    <h4 class="fs-4 flex-grow-1 mb-0"><span class="counter-value" data-target="825">0</span></h4>
-                                    <span class="badge badge-soft-danger fs-12"><i class="ri-arrow-down-s-line fs-13 align-middle me-1"></i>5.02 %</span>
+<div class="container-fluid">
+        <div class="row project-wrapper">
+            <div class="col-xxl-8">
+                <div class="row">
+
+                    <div class="col-xl-4">
+                        <div class="card card-animate">
+                            <div class="card-body">
+                                <div class="d-flex align-items-center">
+                                    <div class="avatar-sm flex-shrink-0">
+                                        <span class="avatar-title bg-soft-primary text-primary rounded-2 fs-2">
+                                            <i data-feather="briefcase" class="text-primary"></i>
+                                        </span>
+                                    </div>
+                                    <div class="flex-grow-1 overflow-hidden ms-3">
+                                        <p class="text-uppercase fw-medium text-muted text-truncate mb-3">Active Projects</p>
+                                        <div class="d-flex align-items-center mb-3">
+                                            <h4 class="fs-4 flex-grow-1 mb-0"><span class="counter-value" data-target="825">0</span></h4>
+                                            <span class="badge badge-soft-danger fs-12"><i class="ri-arrow-down-s-line fs-13 align-middle me-1"></i>5.02 %</span>
+                                        </div>
+                                        <p class="text-muted text-truncate mb-0">Projects this month</p>
+                                    </div>
                                 </div>
-                                <p class="text-muted text-truncate mb-0">Projects this month</p>
+                            </div><!-- end card body -->
+                        </div>
+                    </div><!-- end col -->
+
+                    <div class="col-xl-4">
+                        <div class="card card-animate">
+                            <div class="card-body">
+                                <div class="d-flex align-items-center">
+                                    <div class="avatar-sm flex-shrink-0">
+                                        <span class="avatar-title bg-soft-warning text-warning rounded-2 fs-2">
+                                            <i data-feather="award" class="text-warning"></i>
+                                        </span>
+                                    </div>
+                                    <div class="flex-grow-1 ms-3">
+                                        <p class="text-uppercase fw-medium text-muted mb-3">New Leads</p>
+                                        <div class="d-flex align-items-center mb-3">
+                                            <h4 class="fs-4 flex-grow-1 mb-0"><span class="counter-value" data-target="7522">0</span></h4>
+                                            <span class="badge badge-soft-success fs-12"><i class="ri-arrow-up-s-line fs-13 align-middle me-1"></i>3.58 %</span>
+                                        </div>
+                                        <p class="text-muted mb-0">Leads this month</p>
+                                    </div>
+                                </div>
+                            </div><!-- end card body -->
+                        </div>
+                    </div><!-- end col -->
+
+                    <div class="col-xl-4">
+                        <div class="card card-animate">
+                            <div class="card-body">
+                                <div class="d-flex align-items-center">
+                                    <div class="avatar-sm flex-shrink-0">
+                                        <span class="avatar-title bg-soft-info text-info rounded-2 fs-2">
+                                            <i data-feather="clock" class="text-info"></i>
+                                        </span>
+                                    </div>
+                                    <div class="flex-grow-1 overflow-hidden ms-3">
+                                        <p class="text-uppercase fw-medium text-muted text-truncate mb-3">Fecha & Hora</p>
+                                        <div class="d-flex align-items-center mb-3">
+                                            <h4 class="fs-4 flex-grow-1 mb-0">
+                                                <span id="reloj"></span>
+                                            </h4>
+                                        </div>
+                                        <p class="text-muted text-truncate mb-0"><span id="fecha"></span></p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
 
-            <div class="col-xl-4">
-                <div class="card card-animate">
-                    <div class="card-body">
-                        <div class="d-flex align-items-center">
-                            <div class="avatar-sm flex-shrink-0">
-                                <span class="avatar-title bg-soft-warning text-warning rounded-2 fs-2">
-                                    <i data-feather="award" class="text-warning"></i>
-                                </span>
-                            </div>
-                            <div class="flex-grow-1 ms-3">
-                                <p class="text-uppercase fw-medium text-muted mb-3">New Leads</p>
-                                <div class="d-flex align-items-center mb-3">
-                                    <h4 class="fs-4 flex-grow-1 mb-0"><span class="counter-value" data-target="7522">0</span></h4>
-                                    <span class="badge badge-soft-success fs-12"><i class="ri-arrow-up-s-line fs-13 align-middle me-1"></i>3.58 %</span>
+                </div><!-- end row -->
+
+                <div class="row">
+                    <div class="col-xl-6">
+                        <div class="card">
+                            <div class="card-header border-0 align-items-center d-flex">
+                                <h4 class="card-title mb-0 flex-grow-1">Projects Overview</h4>
+                                <div>
+                                    <button type="button" class="btn btn-soft-secondary btn-sm">
+                                        ALL
+                                    </button>
+                                    <button type="button" class="btn btn-soft-secondary btn-sm">
+                                        1M
+                                    </button>
+                                    <button type="button" class="btn btn-soft-secondary btn-sm">
+                                        6M
+                                    </button>
+                                    <button type="button" class="btn btn-soft-primary btn-sm">
+                                        1Y
+                                    </button>
                                 </div>
-                                <p class="text-muted mb-0">Leads this month</p>
-                            </div>
+                            </div><!-- end card header -->
+
+                            <div class="card-body p-0 pb-2">
+                                <div>
+                                    <div id="projects-overview-chart" data-colors='["--vz-primary", "--vz-warning", "--vz-success"]' dir="ltr" class="apex-charts"></div>
+                                </div>
+                            </div><!-- end card body -->
+                        </div><!-- end card -->
+                    </div><!-- end col -->
+
+                    <div class="col-xl-6">
+                        <div class="card">
+                            <img src="{{ asset('assets/images/logo2.jpg') }}" alt="" height="60" class="img-fluid">
                         </div>
                     </div>
-                </div>
-            </div>
 
-            <div class="col-xl-4">
-                <div class="card card-animate">
-                    <div class="card-body">
-                        <div class="d-flex align-items-center">
-                            <div class="avatar-sm flex-shrink-0">
-                                <span class="avatar-title bg-soft-info text-info rounded-2 fs-2">
-                                    <i data-feather="clock" class="text-info"></i>
-                                </span>
-                            </div>
-                            <div class="flex-grow-1 overflow-hidden ms-3">
-                                <p class="text-uppercase fw-medium text-muted text-truncate mb-3">Fecha & Hora</p>
-                                <div class="d-flex align-items-center mb-3">
-                                    <h4 class="fs-4 flex-grow-1 mb-0">
-                                        <span id="reloj"></span>
-                                    </h4>
-                                </div>
-                                <p class="text-muted text-truncate mb-0"><span id="fecha"></span></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                </div><!-- end row -->
+            </div><!-- end col -->
+        </div><!-- end row -->
+    </div><!-- end container-fluid -->
 
-        <div class="row">
-            <div class="col-xl-12">
-                <div class="card">
-                <img src="{{ asset('assets/images/logo2.jpg') }}" alt="" height="60" class="img-fluid">
-                </div>
-            </div>
-        </div>
-    </div>
-
-@extends('layouts.footer')
+    
 
 <script>
     function actualizarHora() {
@@ -105,6 +140,22 @@
     actualizarHora();
 </script>
 
+    <!-- JAVASCRIPT -->
+    <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/libs/simplebar/simplebar.min.js"></script>
+    <script src="assets/libs/node-waves/waves.min.js"></script>
+    <script src="assets/libs/feather-icons/feather.min.js"></script>
+    <script src="assets/js/pages/plugins/lord-icon-2.1.0.js"></script>
+    <script src="assets/js/plugins.js"></script>
+
+    <!-- apexcharts -->
+    <script src="assets/libs/apexcharts/apexcharts.min.js"></script>
+
+    <!-- projects js -->
+    <script src="assets/js/pages/dashboard-projects.init.js"></script>
+
+    <!-- App js -->
+    <script src="assets/js/app.js"></script>
 
 @endsection
         
