51fb666433ff068a98e4ebb39ffa3a4a04f93aab
Amancayjesus136 <amancayjesus136@gmail.com>
2023-11-15 05:08:57 -0500

    BASE DE DATOS

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,14 +2,14 @@
 
 @section('content')
 <div class="container mt-5">
-    <div class="card shadow">
-        <div class="card-header bg-lg text-black">
-            <h2 class="text-center mb-0">Listado de roles</h2>
+    <div class="card">
+        <div class="card-header bg-primary text-white">
+            <h2 style="color: white;" class="text-center mb-0">ROLES</h2>
         </div>
         <div class="card-body">
             <div class="table-responsive">
-                <table class="table table-bordered table-striped table-hover">
-                    <thead>
+                <table class="table table-bordered table-striped">
+                    <thead class="thead-dark">
                         <tr>
                             <th>ID</th>
                             <th>Nombre</th>
@@ -28,15 +28,15 @@
                                 <form action="{{ route('cambiarRoles', $usuario->id) }}" method="post">
                                     @csrf
                                     @method('PUT')
-                                    @foreach ($roles as $role)
-                                    <div class="form-check">
-                                        <input type="radio" class="form-check-input" name="rol" value="{{ $role->id }}" {{ $usuario->hasRole($role) ? 'checked' : '' }}>
-                                        <label class="form-check-label">{{ $role->name }}</label>
+                                    <div class="form-check form-check-inline">
+                                        @foreach ($roles as $role)
+                                            <input class="form-check-input" type="checkbox" name="rol[]" value="{{ $role->id }}" {{ $usuario->hasRole($role) ? 'checked' : '' }}>
+                                            <label class="form-check-label">{{ $role->name }}</label>
+                                        @endforeach
                                     </div>
-                                    @endforeach
                             </td>
                             <td>
-                                <button type="submit" class="btn btn-primary btn-sm"><i class="fas fa-save"></i> Guardar</button>
+                                <button type="submit" class="btn btn-primary"><i class="fas fa-save"></i> Guardar</button>
                                 </form>
                             </td>
                         </tr>
